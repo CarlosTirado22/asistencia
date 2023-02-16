7911e8cd1d2d309725d27225f0c0ff03c338c36a
David R Delgado <dmdr1015@gmail.com>
2023-02-16 17:49:17 -0500

    primer commit

--- index.php
+++ index.php
@@ -1,11 +1,37 @@
-<?php
-	if (!empty($_SERVER['HTTPS']) && ('on' == $_SERVER['HTTPS'])) {
-		$uri = 'https://';
-	} else {
-		$uri = 'http://';
-	}
-	$uri .= $_SERVER['HTTP_HOST'];
-	header('Location: '.$uri.'/dashboard/');
-	exit;
-?>
-Something is wrong with the XAMPP installation :-(
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title></title>
+    <link rel="stylesheet" type="text/css" href="public/estilos/estilos.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@300;400&display=swap" rel="stylesheet">
+</head>
+<body>
+    <h1>Bienvenido, registra tu asistencia.</h1>
+    <h2 id="fecha"></h2>
+
+    <div class="container">
+        <a class="acceso" href="vista/login/login.php">Ingresar al sistema</a>
+        <p class="dni">Ingrese codigo de estudiante</p>
+
+        <form action="POST">
+            <input type="text" name="txtdni" placeholder="Codigo estudiante">
+            <div class="boton">
+            <a class="entrada" href="#">Registrar Asistencia</a>
+            </div>
+            
+        </form>
+    </div>
+
+    <script>
+        setInterval(() => {
+            let fecha= new Date();
+            let fechaHora= fecha.toLocaleString();
+            document.getElementById("fecha").textContent=fechaHora;
+        }, 1000); 
+    </script>
+</body>
+</html>
---
+++ modelo/conexion.php
@@ -0,0 +1,6 @@
+<?php 
+
+$conexion=new mysqli("localhost", "root", "", "sis_asistencia");
+$conexion->set_charset("utf8");
+
+?>
