992aa3d1920473c4bb5814e732b37d55a9b5330d
Carlos <mariotiradotovar@gmail.com>
2023-02-16 17:12:40 -0500

    primer commit

---
+++ vista/login/login.php
@@ -0,0 +1,79 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <link rel="stylesheet" href="css/bootstrap.css">
+    <link rel="stylesheet" type="text/css" href="css/style.css">
+    <link href="https://fonts.googleapis.com/css?family=Poppins:600&display=swap" rel="stylesheet">
+    <!-- <link rel="stylesheet" href="css/all.min.css"> -->
+    <!-- <link rel="stylesheet" href="css/fontawesome.min.css"> -->
+    <link href="https://tresplazas.com/web/img/big_punto_de_venta.png" rel="shortcut icon">
+    <title>Inicio de sesión</title>
+    <link rel="stylesheet" type="text/css" href="../../public/estilos/login.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
+</head>
+
+<body>
+    <div class="atras">
+        <a href="../../index.php"><svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="currentColor" class="bi bi-arrow-left-circle-fill" viewBox="0 0 16 16">
+        <path d="M8 0a8 8 0 1 0 0 16A8 8 0 0 0 8 0zm3.5 7.5a.5.5 0 0 1 0 1H5.707l2.147 2.146a.5.5 0 0 1-.708.708l-3-3a.5.5 0 0 1 0-.708l3-3a.5.5 0 1 1 .708.708L5.707 7.5H11.5z"/>
+        </svg>
+        </a>
+    </div>
+    <img class="wave" src="img/wave.png">
+    <div class="container">
+        <div class="img">
+            <img src="img/bg.svg">
+        </div>
+        <div class="login-content">
+            <form method="POST" action="">
+                <img src="img/avatar.svg">
+                <h2 class="title">BIENVENIDO</h2>
+                
+                <?php 
+                include "../../modelo/conexion.php";
+                include "../../controlador/login.php";
+                ?>
+                
+                <div class="input-div one">
+                    <div class="i">
+                        <i class="fas fa-user"></i>
+                    </div>
+                    <div class="div">
+                        <h5>Usuario</h5>
+                        <input id="usuario" type="text" class="input" name="usuario"  autocomplete="usuario" value="">
+                    </div>
+                </div>
+                <div class="input-div pass">
+                    <div class="i">
+                        <i class="fas fa-lock"></i>
+                    </div>
+                    <div class="div">
+                        <h5>Contraseña</h5>
+                        <input type="password" id="input" class="input" name="password" autocomplete="current-password">
+                    </div>
+                </div>
+                <div class="view">
+                    <div class="fas fa-eye verPassword" onclick="vista()" id="verPassword"></div>
+                </div>
+
+                <div class="text-center">
+                    <a class="font-italic isai5" href="">Olvidé mi contraseña</a>
+                </div>
+                <input name="btningresar" class="btn" title="click para ingresar" type="submit" value="INICIAR SESION">
+            </form>
+        </div>
+    </div>
+    <script src="js/fontawesome.js"></script>
+    <script src="js/main.js"></script>
+    <script src="js/main2.js"></script>
+    <script src="js/jquery.min.js"></script>
+    <script src="js/bootstrap.js"></script>
+    <script src="js/bootstrap.bundle.js"></script>
+
+</body>
+
+</html>
